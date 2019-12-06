78e6741f56bb2028d3bae813bd949503bf864668
Martin Novák <martin.novak@cleverlance.com>
2019-12-06 13:40:03 +0100

    feat: deploy cloudinary config in API GET

--- examples/getstarted/plugins/envsettings/controllers/EnvSettings.js
+++ examples/getstarted/plugins/envsettings/controllers/EnvSettings.js
@@ -1,4 +1,5 @@
 'use strict';
+var sha256 = require('js-sha256').sha256;
 
 /**
  * EnvSettings.js controller
@@ -12,14 +13,52 @@ module.exports = {
    *
    * @return {Object}
    */
-
   index: async ctx => {
+    // Send 200 `ok`
+    ctx.send({
+      message: 'ok',
+    });
+  },
+
+  getCloudinaryConfig: async ctx => {
     // Add your own logic here.
-    console.log(strapi.config);
+    const {
+      cloudinary: {
+        username = '',
+        apiSecret = '',
+        cloudName = '',
+        apiKey = '',
+      } = {},
+    } = strapi.config;
+
+    const notDefinedInConfig = [username, apiSecret, cloudName, apiKey]
+      .filter(item => item === '')
+      .forEach(item => {
+        console.log(`${item} is not defined in config/cloudinary.json`);
+      });
+
+    var d = new Date();
+    const timestamp = d.getTime();
+
+    let hash = sha256.create();
+    hash.update(
+      `cloud_name=${cloudName}&timestamp=${timestamp}&username=${username}${apiSecret}`
+    );
+
+    const signature = hash.hex();
 
     // Send 200 `ok`
     ctx.send({
       message: 'ok',
+      body: {
+        cloudinary: {
+          cloud_name: cloudName,
+          api_key: apiKey,
+          username,
+          timestamp,
+          signature,
+        },
+      },
     });
   },
 };
